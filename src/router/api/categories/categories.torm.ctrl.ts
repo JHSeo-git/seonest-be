a3ede8aaba2d6a57abe7db5f8cbafef035adac46
JHSeo <qnfqnfqnf@gmail.com>
2021-11-12 23:58:14 +0900

    Update RawCategories api

--- src/router/api/categories/categories.torm.ctrl.ts
+++ src/router/api/categories/categories.torm.ctrl.ts
@@ -1,4 +1,5 @@
 import { Category } from '@src/entity/Category';
+import { PostRead } from '@src/entity/PostRead';
 import { Context } from 'koa';
 import { getRepository, LessThan } from 'typeorm';
 
@@ -40,34 +41,57 @@ export const getRawCategories = async (ctx: Context) => {
     const categoryWithPosts = await getRepository(Category)
       .createQueryBuilder('category')
       .innerJoinAndSelect('category.posts', 'post')
+      .innerJoinAndSelect('post.user', 'user')
       .where('category.url_slug = :slug', { slug })
       .andWhere(cursor ? 'post.id < :cursor' : '1=1', { cursor })
       .orderBy('post.id', 'DESC')
       .limit(takeLatest ?? 10)
       .getRawMany<RawCategory>();
 
-    ctx.body = categoryWithPosts;
+    const categoryWithPostsAndCount = await Promise.all(
+      categoryWithPosts.map(async (post) => {
+        const postCountArr = await getRepository(PostRead)
+          .createQueryBuilder('post_reads')
+          .select('post_reads.ip_hash')
+          .where(`post_reads.post.id = ${post.post_id}`)
+          .groupBy('post_reads.ip_hash')
+          .getRawMany();
+        return {
+          ...post,
+          postReadCount: postCountArr.length,
+        };
+      })
+    );
+
+    ctx.body = categoryWithPostsAndCount;
   } catch (e: any) {
     ctx.throw(500, e);
   }
 };
 
 type RawCategory = {
-  categoryId: number;
-  categoryName: string;
-  categoryUrlSlug: string;
-  categoryCreatedAt: string;
-  categoryUpdatedAt: string;
-  categoryUserId: number;
-  postId: number;
-  postTitle: string;
-  postBody: string;
-  postShortDescription: null;
-  postThumbnail: null;
-  postUrlSlug: string;
-  postIsTemp: number;
-  postReadTime: number;
-  postCreatedAt: string;
-  postUpdatedAt: string;
-  postUserId: number;
+  category_id: number;
+  category_name: string;
+  category_url_slug: string;
+  category_created_at: string;
+  category_updated_at: string;
+  category_user_id: number;
+  post_id: number;
+  post_title: string;
+  post_body: string;
+  post_short_description: string | null;
+  post_thumbnail: string | null;
+  post_url_slug: string;
+  post_is_temp: boolean;
+  post_read_time: number;
+  post_created_at: string;
+  post_updated_at: string;
+  post_user_id: number;
+  user_id: number;
+  user_email: string;
+  user_display_name: string;
+  user_photo_url: string | null;
+  user_is_admin: boolean;
+  user_created_at: string;
+  user_updated_at: string;
 };
